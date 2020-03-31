b87b3a1f9f36ae307507acd6c51b162737ae40c4
Shivarpiya <44540060+Shivarpiya@users.noreply.github.com>
2020-03-31 11:37:42 +0530

    Update anchore.sh

--- anchore.sh
+++ anchore.sh
@@ -1,4 +1,6 @@
 echo "starting....."
+aws_access_key=`cat credentials | grep -i "aws_access_key_id" | awk '{print $3}'`
+aws_secret_access_key=`cat credentials | grep -i "aws_secret_access_key" | awk '{print $3}'`
 pwd
 cd /root/aevolume
 docker-compose up -d
@@ -11,5 +13,8 @@ pip install --user --upgrade anchorecli
 docker-compose exec engine-api anchore-cli system status
 docker-compose exec engine-api anchore-cli system feeds list
 anchore-cli image list
-anchore-cli --u admin --p foobar --url http://localhost:8228/v1 image add openjdk:11-jdk
-anchore-cli --u admin --p foobar --url http://localhost:8228/v1 image vuln openjdk:11-jdk all
+#anchore-cli --u admin --p foobar --url http://localhost:8228/v1 image add openjdk:11-jdk
+#anchore-cli --u admin --p foobar --url http://localhost:8228/v1 image vuln openjdk:11-jdk all
+anchore-cli --u admin --p foobar --url http://localhost:8228/v1 registry add 828047571549.dkr.ecr.us-east-2.amazonaws.com/ecr-repo-sample $aws_access_key $aws_secret_access_key --registry-type=awsecr
+anchore-cli --u admin --p foobar --url http://localhost:8228/v1 image add 828047571549.dkr.ecr.us-east-2.amazonaws.com/ecr-repo-sample:latest
+anchore-cli --u admin --p foobar --url http://localhost:8228/v1 image vuln 828047571549.dkr.ecr.us-east-2.amazonaws.com/ecr-repo-sample:latest all
